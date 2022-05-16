7e80a7e3b2e9b4da5c06aacc5e44c0cbbfe7708b
Maxmarra <5919669@gmail.com>
2022-05-16 10:38:57 +0300

    part-1

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -31,6 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+
                     TipTimeScreen()
                 }
             }
